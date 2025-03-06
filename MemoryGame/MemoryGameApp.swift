43ca1176bd4e24843df9b50e06388d680efa1ccb
Jason Greer <jasongreer08@gmail.com>
2025-03-05 21:20:38 -0600

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Jason Greer on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
